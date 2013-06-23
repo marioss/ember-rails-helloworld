7814d8225b91d87962b3e4ad79b31a2410576187
Mario Stefano Suharja <me@mario.sg>
2013-06-23 11:45:43 +0700

    Initialize Rails App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Railsonmaui::Application.config.secret_key_base = 'e7b06f3811822bf4cf45f1223e42c7d4fd88a852c948015487107c51af4a29e11ae2518e0bfa4e7fe162a7b7db522d74299aa93bc76305249be48aa24bb1641b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Railsonmaui::Application.config.session_store :cookie_store, key: '_railsonmaui_session'
